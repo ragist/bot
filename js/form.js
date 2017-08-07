2e240f4ebcdcd80d7fc31daf54bf3c4b2836110f
ragist <cypherian09@gmail.com>
2017-08-07 09:41:58 +0000

    New

--- js/form.js
+++ js/form.js
@@ -1,28 +1,15 @@
-document.getElementById('submit').addEventListener('click', sendForm);
-
-function sendForm(e) {
-	e.preventDefault();
-    const form = document.getElementById('form');
-    if (form.checkValidity()) {
-    	alert('Form is OK');  
-    } else {
-    	alert('Form is not OK');
-    }   
-}
-
-$(function() {
-    $('form input[type=submit]').click(sendForm);
-});
-
-function sendForm(e) {
-    e.preventDefault();
-    $.ajax({
-        url: "https://formspree.io/cypherian09@gmail.com", 
-        method: "POST",
-        data: {
-            email: $('#email').val(),
-            
-        },
-        dataType: "json"
-    });
-}
+function call() {
+ 	  var msg   = $('contact-form').serialize();
+        $.ajax({
+          type: 'POST',
+          url: 'http://formspree.io/cypherian09@gmail.com',
+          data: msg,
+          success: function(data) {
+            $('#results').html(data);
+          },
+          error:  function(xhr, str){
+	    alert('Возникла ошибка: ' + xhr.responseCode);
+          }
+        });
+ 
+    }
